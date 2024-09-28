d9e94f60ad45938ea180b936b8280c967834d5b5
IYSTREEM <97679391+IYSTREEM@users.noreply.github.com>
2024-09-28 10:41:16 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Update and upgrade the system
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install Git
+sudo apt install git -y
+
+# Clone the Docker install repository
+git clone https://github.com/dnburgess/dockerinstall.git
+
+# Navigate to the cloned directory
+cd dockerinstall
+
+# Make the install script executable and run it
+chmod +x dockerinstall.sh
+sudo ./dockerinstall.sh
+
+# Navigate to /tmp directory
+cd /tmp
+
+# Download Kasm release
+curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.16.0.f2d6e1.tar.gz
+
+# Extract the tarball
+tar -xf kasm_release_1.16.0.f2d6e1.tar.gz
+
+# Run the Kasm install script
+sudo bash kasm_release/install.sh
